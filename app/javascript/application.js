da40a10331de9617a768065dff4c1d208f281add
Ivan Ordonez <ivanord@gmail.com>
2023-06-22 08:48:03 -0500

    created Pins scaffold edited views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+require jquery //this solved a f problem when the dropdown menu was not working
+require bootstrap.js
+require bootstrap.css
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :pins
   devise_for :users
   
   root "home#index"
   get 'home/about'
+  #get 'users/sign_out'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
